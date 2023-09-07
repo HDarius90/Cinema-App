01316bf55e802422854af2ae004548ed2af51324
Darius Hadjati <darius.hadjati@gmail.com>
2023-09-07 17:32:46 +0200

    creating the basic css files and creating new components

--- src/components/roomUI/Line.jsx
+++ src/components/roomUI/Line.jsx
@@ -1,13 +1,16 @@
 import Chair from "./Chair";
+import "./Line.css";
 
 export default function Line({ seatNumbers, lineNumber }) {
-  console.log(seatNumbers);
+  const chairs = new Array(seatNumbers)
+    .fill()
+    .map((_, index) => (
+      <Chair key={index + seatNumbers * lineNumber} seatNumber={index + 1} />
+    ));
   return (
     <div className="line">
-        <span>{lineNumber}</span>
-      {seatNumbers.map((seatNumber) => (
-        <Chair seatNumber={seatNumber} key={seatNumber} />
-      ))}
+      <span>{lineNumber}</span>
+      <div className="chairs">{chairs}</div>
       <span>{lineNumber}</span>
     </div>
   );
--- src/components/roomUI/SeatPlanContainer.jsx
+++ src/components/roomUI/SeatPlanContainer.jsx
@@ -3,13 +3,17 @@ import Line from "./Line";
 export default function SeatPlanContainer() {
   return (
     <div id="SeatPlanContainer">
-      <Line seatNumbers={[1, 2, 3, 4, 5, 6, 7, 8]} lineNumber={1} />
-      <Line seatNumbers={[9, 10, 11, 12, 13, 14, 15, 16]} lineNumber={2} />
-      <Line seatNumbers={[17, 18, 19, 20, 21, 22, 23, 24]} lineNumber={3} />
-      <Line seatNumbers={[25, 26, 27, 28, 29, 30, 31, 32]} lineNumber={4} />
-      <Line seatNumbers={[33, 34, 35, 36, 37, 38, 39, 40]} lineNumber={5} />
-      <Line seatNumbers={[41, 42, 43, 44, 45, 46, 47, 48]} lineNumber={6} />
-      <Line seatNumbers={[49, 50, 51, 52, 53, 54, 55, 56]} lineNumber={7} />
+      <Line seatNumbers={20} lineNumber={1} />
+      <Line seatNumbers={20} lineNumber={2} />
+      <Line seatNumbers={20} lineNumber={3} />
+      <Line seatNumbers={20} lineNumber={4} />
+      <Line seatNumbers={20} lineNumber={5} />
+      <Line seatNumbers={20} lineNumber={6} />
+      <Line seatNumbers={20} lineNumber={7} />
+      <Line seatNumbers={20} lineNumber={8} />
+      <Line seatNumbers={20} lineNumber={9} />
+      <Line seatNumbers={20} lineNumber={10} />
+
     </div>
   );
 }
